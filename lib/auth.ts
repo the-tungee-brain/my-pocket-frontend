20779a419f75902897f07799e569c5c8c015c053
Tung Dinh <tungxd301@gmail.com>
2026-03-13 02:05:12 -0700

    Add callback

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,8 +9,20 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async signIn({account, profile}) {
-      console.log(account, profile)
+      if (account?.provider === 'google' && account.id_token) {
+      const res = await fetch(
+        'https://thetungeebrain.duckdns.org/api/v1/auth/google/callback',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ id_token: account.id_token }),
+        },
+      )
+
+      if (!res.ok) return false
       return true
+    }
+    return true
     }
   }
 })
