9dba470cc36b3ec82e61c0a1d574066a46f69fbb
Tung Dinh <tungxd301@gmail.com>
2026-03-21 02:53:46 -0700

    Changes

--- components/AuthPage.tsx
+++ components/AuthPage.tsx
@@ -4,8 +4,8 @@ import { Button } from "@/components/ui/Button";
 
 export default function AuthPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
         <Button
           onClick={() => signIn("google")}
           className="sm:w-auto"
