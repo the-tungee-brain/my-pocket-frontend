6210b971caa64cfdfc1379fa3e02bdbcef7b0b32
Tung Dinh <tungxd301@gmail.com>
2026-03-28 11:18:53 -0700

    changes

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,7 +14,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
   session: { strategy: 'jwt' },
   debug: true,
   logger: {
